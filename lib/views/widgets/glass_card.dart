85783c8d9da8a6f842f3f8a4e39194b2517ade7d
Aflah Shadil k <shadilpml@gmail.com>
2026-03-18 10:39:28 +0530

    glass card

---
+++ lib/views/widgets/glass_card.dart
@@ -0,0 +1,22 @@
+import 'package:financialkeeper/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+
+  const GlassCard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppColors.glass,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: AppColors.border),
+      ),
+      child: child,
+    );
+  }
+}
